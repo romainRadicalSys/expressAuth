79f6f94940a137446a6931e109a46a2697136b33
romain <romain@radicalsys.com>
2018-02-23 14:01:36 +0000

    mains routes set

---
+++ helper.js
@@ -0,0 +1,12 @@
+/*
+    File of helper functions or data that can be use in our templating function
+*/
+
+//moment.js
+exports.moment = require("moment");
+
+//Data for the site
+
+exports.siteName = `Express Authentication DEMO`;
+
+exports.menu = [{ slug: "/index", title: "index", icon: "home" }, { slug: "/about", title: "about", icon: "about" }, { slug: "/contact", title: "contact", icon: "contact" }, { slug: "/profile", title: "profile", icon: "profile" }, { slug: "/signup", title: "signup", icon: "signup" }];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+	res.render("index", {
+		title: "Express",
+		name: "romain"
+	});
+});
+
+router.get("/about", function(req, res, next) {
+	res.render("about", { title: "about" });
+});
+
+router.get("/signup", (req, res) => {
+	res.render("signup", {
+		title: "Sign-up"
+	});
 });
 
 module.exports = router;
